904623be6059ba9079af886f7ddf38204611d3f1
john <sebastian80_23@hotmail.com>
2021-03-15 09:52:45 -0500

    Adding invoice

--- models/migration.py
+++ models/migration.py
@@ -534,6 +534,7 @@ class MigrationModel(models.Model):
         # account
         # tax
 
+
         account_move = self.env['account.move']
         partner_model = self.env['res.partner']
         res_user_model = self.env['res.users']
